477c9e6ac07ae8cd83a2f5ad414562bed9f755cc
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-26 17:04:13 -0500

    initial

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const helmet = require('helmet');
+const router = require('express').Router();
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+
+  }
+}
+const db = knex(knexConfig)
+
 // endpoints here
+
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
