28a4b985281076cb44d569a0bee922a0a0a40b29
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-26 17:23:49 -0500

    post and get completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const router = require('express').Router();
+// const router = Router();
 const knex = require('knex');
 
 const server = express();
@@ -18,8 +18,40 @@ const knexConfig = {
 }
 const db = knex(knexConfig)
 
-// endpoints here
+////////// ENDPOINTS //////////
 
+//POST 
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+  .insert(req.body)
+  .then(zooId => {
+    const [id] = zooId
+    db('zoos')
+    .where({ id })
+    .first()
+    .then(zoo => {
+      res.status(200).json(zoo)
+    })
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+
+
+
+
+// GET
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
+});
 
 const port = 3300;
 server.listen(port, function() {
